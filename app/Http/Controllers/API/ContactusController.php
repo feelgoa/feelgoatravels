d5f00c0153a65f39a0188a89febf71b30dbdb475
upshift4fun <alisther1627@gmail.com>
2020-06-08 00:24:59 +0530

    removed unused code

--- app/Http/Controllers/API/ContactusController.php
+++ app/Http/Controllers/API/ContactusController.php
@@ -11,57 +11,35 @@ use DB;
 
 class ContactusController extends Controller {
 
-	public function savedetails() {
-		if(isset($_POST['g-recaptcha-response'])){
-			$captcha=$_POST['g-recaptcha-response'];
-		  }
-		  if(!$captcha){
-			return response()->json(['isSuccess'=> false, 'message'=> RECAPTCH_REQUIRED,'data'=> '']);
-		  }
-		  /*$secretKey = RECAPCHA_SECRET_KEY;
-		  $ip = $_SERVER['REMOTE_ADDR'];
-		  // post request to server
-		  $url = 'https://www.google.com/recaptcha/api/siteverify?secret=' . urlencode($secretKey) .  '&response=' . urlencode($captcha);
-		  $response = file_get_contents($url);
-		  $responseKeys = json_decode($response,true);*/
-		  $responseKeys = validate_recapcha(urlencode($captcha));
-		  // should return JSON with success as true
-		  if($responseKeys["success"]) {
-			  return response()->json(['isSuccess'=> true, 'message'=> 'Your Data has been submited successfully.','data'=> '']);
-				  
-		  } else {
-			  return response()->json(['isSuccess'=> false, 'message'=> 'Suspecious User.','data'=> '']);
-		  }
-	}
 	public function save_contact_us_form(Request $request) {
 		$validator = Validator::make($request->all(), [ 
 			'firstname' => 'required', 
 			'lastname' => 'required', 
 			'email' => 'required|email', 
 			'message' => 'required',
-        ]);
+		]);
 		if ($validator->fails()) { 
 			return response()->json(['isSuccess'=> false, 'message'=> VALIDATION_FAILED, 'error'=> $validator->errors(), 'data'=> '']);
-        }
+		}
 		try {
-            $captcha = "";
+			$captcha = "";
 			if(isset($_POST['g-recaptcha-response'])){
 				$captcha=$_POST['g-recaptcha-response'];
-            }
+			}
 
 			if(!$captcha){
 				return response()->json(['isSuccess'=> false, 'message'=> RECAPTCH_REQUIRED,'data'=> '']);
-            }
-            $responseKeys = validate_recapcha(urlencode($captcha));
-            $responseKeys["success"]  = 1;
+			}
+			$responseKeys = validate_recapcha(urlencode($captcha));
+			$responseKeys["success"]  = 1;
 			if($responseKeys["success"] == 1) {
-                $input = $request->all();
-                $name_concat = $input['firstname'].' '.$input['lastname'];
-                $mailsender = send_mail_custom($input['email'],$name_concat);
-                if ($mailsender == 1) {
-                    unset($input["_token"]);
-                    unset($input["g-recaptcha-response"]);
-                    DB::table('contactus')->insert($input);
+				$input = $request->all();
+				$name_concat = $input['firstname'].' '.$input['lastname'];
+				$mailsender = send_mail_custom($input['email'],$name_concat);
+				if ($mailsender == 1) {
+					unset($input["_token"]);
+					unset($input["g-recaptcha-response"]);
+					DB::table('contactus')->insert($input);
 					return response()->json(['isSuccess'=> true, 'message'=> FORM_DATA_SUBIMITTED_CONTACT_US_FORM,'data'=> '']);
 				} else {
 					return response()->json(['isSuccess'=> false, 'message'=> FORM_SUBMIT_FAILED, 'error'=> EMAIL_SENDING_ERROR, 'data'=> '']);
