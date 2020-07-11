496b30deb33c63858e321e58cfa65a7b474bcfcc
Sayyed Mohammed Arshad <arshad.sayed3385@gmail.com>
2020-07-11 16:20:14 +0530

    updated bookings.blade.php

--- resources/views/user/bookings.blade.php
+++ resources/views/user/bookings.blade.php
@@ -41,68 +41,18 @@
 							</div>
 						</div>
 					</div>
-<<<<<<< Updated upstream
-					<div class="col-sm-6 team_details">
-							<label for="age" class="frm-font lable-from">Age:</label>
-							<span class="required-field">*</span>
-							<input type="text" class="frm-font form-control" maxlength="2" id="age" name="age" placeholder="Enter Age" onblur="ValidateEmptyNumberField(this,'Age must be integer and cannot be empty')" required>
-					</div>
-				</div>
-			</div>
-			<div class="form-group">
-				<div class="row">
-					<div class="col-sm-4 team_details">
-						<label for="place" class="frm-font lable-from">Place of Origin:</label>
-						<span class="required-field">*</span>
-						<input name="place" type="text" class="frm-font form-control" id="place" placeholder="Enter place of origin" onblur="ValidateEmptyField(this,'Place oF origin cannot be empty')" required>
-					</div>
-					<div class="col-sm-4 team_details">
-						<label for="number" class="frm-font lable-from">Total Male Members:</label>
-						<input name="male_count" type="text" maxlength="2" value="0" class="frm-font form-control" id="male_count" placeholder="Male Members count" required>
-					</div>
-					<div class="col-sm-4 team_details">
-						<label for="number" class="frm-font lable-from">Total Female Members:</label>
-						<input name="female_count" type="text" maxlength="2" value="0" class="frm-font form-control" id="female_count" placeholder="Female Members count"  required>
-					</div>
-				</div>
-			</div>
-			<span class="label label-danger" style="font-size: 14px;margin-bottom:14px;white-space: normal;" id="error_message"></span></br>
-		</div>
-		<div data-panel="two-s1" class="div-s1">
-			<h2 class="lable-from">Package Details</h2>
-				<div class="row">
-					<div class="spot-container">
-						<h4 class="lable-from">Exclusive North Goa Tour( Day 1)</h4>
-						<div class="form-group">
-							<label for="travel_date1" class="frm-font lable-from">Travelling Date:</label>
-							<span class="required-field">*</span>
-							<input type="date" class="frm-font form-control travel_dates" name="travel_date1" id="travel_date1" onblur="CheckSameDate(this,'Travelling Date should be one day ahead from today and different from other travelling dates','error_message1')" required>
-						</div>
-					</div>
-				
-					<div class="col-sm-6">
-						<h6 class="lable-from">Included in Package</h6>
-						<div class="form-group">
-						@foreach ($package_details as $spot)
-							@if($spot->zone=="North Goa" and $spot->extra_info=="" and $spot->day=="1")
-							<div class="checkbox" style="padding-left: 1.5rem;margin-top: 10px;">
-								<label class="lable-from">
-								<input type="checkbox" class="tours" id="{{$spot->id}}" name="spots[]" value="{{$spot->id}}">
-								{{$spot->spot_name}}
-								</label>
-=======
 					<div class="form-group">
 						<div class="row">
 							<div class="col-sm-6 team_details">
 								<label for="gender" class="frm-font lable-from">Gender:</label><span class="required-field">*</span><br>
 								<div class="form-check" style="display: inline-flex;">
 									<div class="form-check" style="padding-right:12px;">
-									<input class="form-check-input" type="radio" name="gender" id="b1" value="male" required>
-									<label class="form-check-label lable-from" for="b1">Male</label>
+									  <input class="form-check-input" type="radio" name="gender" id="b1" value="male" required>
+									  <label class="form-check-label lable-from" for="b1">Male</label>
 									</div>
 									<div class="form-check">
-									<input class="form-check-input" type="radio" name="gender" id="b2" value="female" required>
-									<label class="form-check-label lable-from" for="b2">Female</label>
+									  <input class="form-check-input" type="radio" name="gender" id="b2" value="female" required>
+									  <label class="form-check-label lable-from" for="b2">Female</label>
 									</div>
 								</div>
 							</div>
@@ -110,7 +60,6 @@
 									<label for="age" class="frm-font lable-from">Age:</label>
 									<span class="required-field">*</span>
 									<input type="text" class="frm-font form-control" maxlength="2" id="age" name="age" placeholder="Enter Age" onblur="ValidateEmptyNumberField(this,'Age must be integer and cannot be empty')" required>
->>>>>>> Stashed changes
 							</div>
 						</div>
 					</div>
@@ -131,29 +80,8 @@
 							</div>
 						</div>
 					</div>
-					<span class="label label-danger" style="font-size: 14px;margin-bottom:14px;white-space: normal;" id="error_message"></span></br>
+				<span class="label label-danger" style="font-size: 14px;margin-bottom:14px;white-space: normal;" id="error_message"></span></br>
 				</div>
-<<<<<<< Updated upstream
-				<div class="row">
-				    <div class="spot-container">
-						<h4 class="lable-from">Exclusive South Goa Tour (Day 2)</h4>
-						<div class="form-group">
-							<label for="travel_date2" class="frm-font lable-from">Travelling Date:</label>
-							<span class="required-field">*</span>
-							<input type="date" class="frm-font form-control travels_dates" name="travel_date2" id="travel_date2" onblur="CheckSameDate(this,'Travelling Date should be one day ahead from today and different from other travelling dates','error_message1')" required>
-						</div>
-					</div>
-					<div class="col-sm-6">
-						<h6 class="lable-from">Included in Package</h6>
-						<div class="form-group">
-							@foreach ($package_details as $spot)
-							@if($spot->zone=="South Goa" and $spot->extra_info=="" and $spot->day=="2")
-							<div class="checkbox" style="padding-left: 1.5rem;margin-top: 10px;">
-								<label class="lable-from">
-								<input type="checkbox" class="tours" id="{{$spot->id}}" name="spots[]" value="{{$spot->id}}">
-								{{$spot->spot_name}}
-								</label>
-=======
 				<div data-panel="two-s1" class="div-s1">
 					<h2 class="lable-from">Package Details</h2>
 					<div class="row">
@@ -195,7 +123,6 @@
 										@endif
 									@endforeach
 								</div>
->>>>>>> Stashed changes
 							</div>
 						</div>
 						<div class="col-sm-6">
@@ -239,48 +166,6 @@
 							</div>	
 						</div>
 						<span class="label label-danger" id="error_message1" style="margin-left:2%;"></span><br>	
-					</div>
-<<<<<<< Updated upstream
-				</div>
-				<label class="lable-from">Travel type: </label>
-				<span class="required-field">*</span><br>
-				<div class="radio-form2">
-					<input class="form-check-input" type="radio" name="package_type" id="p1" value="single" required>
-					<label class="form-check-label lable-from" for="p1">Single</label>
-				</div>
-				<div class="radio-form2">
-					<input class="form-check-input" type="radio" name="package_type" id="p2" value="couple" required>
-					<label class="form-check-label lable-from" for="p2">Couple</label>
-				</div>
-				<div class="radio-form2">
-					<input class="form-check-input" type="radio" name="package_type" id="p3" value="familyless5" required>
-					<label class="form-check-label lable-from" for="p3">Family (Less than 5)</label>
-				</div>
-				<div class="radio-form2">
-					<input class="form-check-input" type="radio" name="package_type" id="p4" value="group" required>
-					<label class="form-check-label lable-from" for="p4">Group</label>
-				</div>
-			<div class="alert alert-warning" role="alert">
-			  <strong>Note:</strong> For more info on places you can click <a style="color:#CE3232;" href="/locations" target="_blank">here</a>.
-			</div>
-			<span class="label label-danger" id="error_message1"></span><br>
-		</div>
-		<div data-panel="three-s1" class="div-s1">
-			<h2 class="lable-from">Extra Package for Group</h2>
-			<div class="row">
-					<div class="col-sm-6">
-					    <h4 class="lable-from">Day 3</h4>
-						<div class="form-group">
-							<label for="travel_date3" class="frm-font lable-from">Travelling Date:</label>
-							<span class="required-field">*</span>
-							<input type="date" class="frm-font form-control travelling_dates" name="travel_date3" id="travel_date3" onblur="CheckSameDate(this,'Travelling Date should be one day ahead from today and different from other travelling dates','error_message2'); checkday3places();">
-=======
-					<div class="row">
-						<div class="optional-info">
-							<h4 class="lable-from">Extra Places (Optional)</h4>
-							<p style="color: #FFC107;"><strong>Note:</strong> These places are optional and are not included in the package. Extra Charges will be apply. When less than 5 people cab will be provided</p>	
->>>>>>> Stashed changes
-						</div>
 						<div class="col-sm-6">
 							<div class="spot-container">
 								<h4 class="lable-from">Day 3</h4>
@@ -304,19 +189,6 @@
 								@endforeach
 								</div>
 							</div>
-<<<<<<< Updated upstream
-							@endif
-						@endforeach
-						</div>
-					</div>
-					<div class="col-sm-6">
-						<h4 class="lable-from">Day 4</h4>
-						<div class="form-group">
-							<label for="travel_date4" class="frm-font lable-from">Travelling Date:</label>
-							<span class="required-field">*</span>
-							<input type="date" class="frm-font form-control travelling_dates" name="travel_date4" id="travel_date4" onblur="CheckSameDate(this,'Travelling Date should be one day ahead from today and different from other travelling dates','error_message2')" checkday4places();>
-=======
->>>>>>> Stashed changes
 						</div>
 						<div class="col-sm-6">
 							<div class="spot-container">
@@ -397,7 +269,6 @@
 									</div>
 								</div>
 							</div>
-							
 						</div>
 					</div>
 					<div class="row">
@@ -442,34 +313,7 @@
 							<span class="label label-danger" id="error_message1"></span><br>
 							<input type = 'submit' class="btn btn-success" value = "Submit Booking Details"/>
 						</div>
-<<<<<<< Updated upstream
-					  </div>
-				</div>
-				<div class="row">
-					<div class="col-sm-12">
-						</br>
-						<div class="col-md-12 colourbanner">
-							<h3 class="colorlight" style="margin-top: 0px;">TERMS AND CONDITIONS</h3>
-						</div>
-						</br>
-						<div class="lable-from">
-							@foreach ($terms_conditions as $t_c)
-								{!! $t_c->content !!}
-							@endforeach
-						</div>
 					</div>
-					<input type="checkbox"  name="terms" id="terms" value="True" required style="margin-left: 12px;width: 20px;height: 20px;">
-					<label class="lable-from" for="terms" style="margin-left: 2%;font-size:14px;">I agree to these terms and conditions: </label><span class="required-field">*</span>
-				</div>
-			</div>
-			<div class="form-group" style="display:none">
-				<div class="checkbox">
-					<label for="hotelbooking lable-from">
-					<input type="checkbox" id="hotelbooking" name="hotelbooking" value="yes">
-					Book Hotel</label>
-=======
-					</div>
->>>>>>> Stashed changes
 				</div>
 			</form>
 		</div>
